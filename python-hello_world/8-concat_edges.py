1d01b03754fa12f5b52c1f17e85de3331d0cbe1d
Turqut <turqutismayil007@gmail.com>
2026-03-08 19:02:38 +0000

    Fix 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str2 = "with Python"
-print(str1[39:66] + str2)
+print(str[39:66] + " " + str[107:113] + str[66:107])
