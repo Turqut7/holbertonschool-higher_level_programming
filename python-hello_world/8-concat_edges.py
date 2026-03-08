e8b71709635d247d8609a2985c7a1506f30109ac
Turqut <turqutismayil007@gmail.com>
2026-03-08 18:59:58 +0000

    Fix 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-word = "Holberton"
-print(word[3:8], word[:2] + word[8:], word[2:8] + word[-2:] + word[:6])
+str1 = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str2 = "with Python"
+print(str1[39:66] + str2)
