cefa1515775407d0dd1512f9b9d02ba2b73a7935
Turqut <turqutismayil007@gmail.com>
2026-03-08 18:56:19 +0000

    Add 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(word[5:] + chr(32) + word[:2] + word[8:] + chr(32) + word[2:8] + word[-2:] + word[:6])
+print(word[3:8], word[:2] + word[8:], word[2:8] + word[-2:] + word[:6])
