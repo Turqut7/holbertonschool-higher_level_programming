597ba06d0e2436ec71cb8cd91f4d4b130614748f
Turqut <turqutismayil007@gmail.com>
2026-03-20 19:46:34 +0000

    Fix the files

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,7 +6,5 @@ class MyList(list):
     """A class that inherits from list."""
 
     def print_sorted(self):
-        """Prints the list, but sorted (ascending sort).
-        Assumes all elements in the list are of the same type (integers).
-        """
+        """Prints the list, but sorted."""
         print(sorted(self))
