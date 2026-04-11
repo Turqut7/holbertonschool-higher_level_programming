9f769d2af0c7522c0f3bc45469a198c0d042318f
Turqut <turqutismayil007@gmail.com>
2026-04-11 09:22:42 +0000

    File fixed

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ".format(sys.argv[4])
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ".format(sys.argv[4])
     query += "ORDER BY id ASC"
     cur.execute(query)
 
